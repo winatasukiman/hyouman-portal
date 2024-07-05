75774a0cd6019fe5b7be9709630cafbcc15fba97
victor.kartika <victor.kartika@thehyouman.com>
2024-07-05 08:47:02 +0700

    feat: Add module web_custom - To change Odoo main theme color to #32658D

---
+++ odoo17-custom/web_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Hyouman Web Custom',
+    'category': '',
+    'sequence': 54,
+    'summary': 'Hyouman Web Custom',
+    'version': '1.0',
+    'description': """""",
+    'depends': [
+    ],
+    'data': [
+    ],
+    'assets': {
+        'web._assets_primary_variables': [
+            ('prepend', 'web_custom/static/src/scss/primary_variables.scss'),
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
