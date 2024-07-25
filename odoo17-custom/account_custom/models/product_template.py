0a20f48b2f6bd9c5536185afd121727323e28927
victorimannuel <victorimannuel.dev@gmail.com>
2024-07-25 17:09:27 +0700

    feat: Change product type default value - Change from consu to service

--- odoo17-custom/account_custom/models/product_template.py
+++ odoo17-custom/account_custom/models/product_template.py
@@ -4,5 +4,13 @@ from odoo import models, fields
 class ProductTemplateCustom(models.Model):
     _inherit = 'product.template'
 
-    # Override field to set default=False
+    # Override field to set False as the default value
     purchase_ok = fields.Boolean('Can be Purchased', default=False)
+
+    # Override field to set 'service' as the default value
+    detailed_type = fields.Selection([
+        ('consu', 'Consumable'),
+        ('service', 'Service')], string='Product Type', default='service', required=True,
+        help='A storable product is a product for which you manage stock. The Inventory app has to be installed.\n'
+             'A consumable product is a product for which stock is not managed.\n'
+             'A service is a non-material product you provide.')
