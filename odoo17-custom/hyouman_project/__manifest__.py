8d9848001483ed9d5f8d85c8a1236c75effadcd4
victorimannuel <victorimannuel.dev@gmail.com>
2025-05-27 01:00:55 +0700

    feat(project): custom odoo project standard module - modify project.task action domain - modify project.project kanban card view

---
+++ odoo17-custom/hyouman_project/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Hyouman - Project",
+    'summary': """
+        Hyouman custom project module for Odoo 17
+    """,
+    'description': """
+        Hyouman custom project module for Odoo 17
+    """,
+    'author': "Victor Imannuel",
+    'category': 'Project',
+    'version': '17.01',
+
+    'depends': [
+        'project',
+    ],
+
+    'data': [
+        # Views
+        'views/project_views.xml',
+    ],
+}
