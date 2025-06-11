f53818f5f613ff3b8e2dbac6fbb6fe8c6273a2c4
victorimannuel <victorimannuel.dev@gmail.com>
2025-06-11 20:56:32 +0700

    [IMP] hyouman_project: remove deprecated code

--- odoo17-custom/hyouman_project/models/project_task_type.py
+++ odoo17-custom/hyouman_project/models/project_task_type.py
@@ -12,25 +12,3 @@ class ProjectTask(models.Model):
             tasks = self.env['project.task'].search([('stage_id', '=', record.id)])
             record.task_count = len(tasks)
             record.done_task_count = len(tasks.filtered(lambda t: t.state in ['1_done']))
-            
-    @api.model_create_multi
-    def create(self, vals_list):
-        res = super().create(vals_list)
-
-        for task in res:
-            related_stage_goals = self.env['goal.goal'].search([]).filtered(lambda goal: task.stage_id in goal.related_section_ids)
-            for goal in related_stage_goals:
-                goal._compute_completion_percentage()
-        
-        return res
-
-    def write(self, vals):
-        res = super().write(vals)
-        
-        if 'state' in vals:
-            print(vals)
-            related_stage_goals = self.env['goal.goal'].search([]).filtered(lambda goal: self.stage_id in goal.related_section_ids)
-            for goal in related_stage_goals:
-                goal._compute_completion_percentage()
-            
-        return res
