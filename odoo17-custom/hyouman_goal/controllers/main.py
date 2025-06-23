5397b2beadf475d4e8ada790551a0fa8b46290f2
victorimannuel <victorimannuel.dev@gmail.com>
2025-06-23 13:58:51 +0700

    [IMP] hyouman_goal: Beautify strategy map Requested by Hyouman (Ms. Cessa)

--- odoo17-custom/hyouman_goal/controllers/main.py
+++ odoo17-custom/hyouman_goal/controllers/main.py
@@ -6,14 +6,25 @@ class GoalController(http.Controller):
     """ Controller for handling goal-related requests. """
 
     @http.route('/goal/graph', auth='user', type='json')
-    def get_goal_graph(self):
+    def get_goal_graph(self, team_name):
         """ Returns a JSON representation of the goal graph. """
-        return self._get_goal_graph()
-
-    def _get_goal_graph(self):
-        """ Helper method to construct the goal graph data. """
-        goals = request.env['goal.goal'].sudo().search([])
+        goals = request.env['goal.goal'].sudo().search([('accountable_team', '=', team_name)])
         nodes = [{'id': goal.id, 'label': goal.name, 'type': 'parent' if goal.sub_goal_ids else 'sub'} for goal in goals]
         links = [{'source': p.id, 'target': c.id} for c in goals for p in c.parent_goal_ids]
 
         return {'nodes': nodes, 'links': links}
+    
+    @http.route('/goal/team_list', auth='user', type='json')
+    def get_goal_team(self):
+        """ Returns a JSON representation of the goal graph. """
+        goals = request.env['goal.goal'].sudo().search([])
+        team_data = []
+        for index, goal in enumerate(goals):
+            team = goal.accountable_team
+            if team and team not in [t['name'] for t in team_data]:
+                team_data.append({
+                    'id': index,
+                    'name': team,
+                })
+               
+        return team_data
